269333d06ab43bff24286d7ee2d0f548f863ef35
Dan <danilviktar@gmail.com>
2024-03-26 15:21:12 +0200

    Fix doubling Countries in adding

--- app/src/main/java/com/example/country/CountryDao.java
+++ app/src/main/java/com/example/country/CountryDao.java
@@ -3,6 +3,7 @@ package com.example.country;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Dao
 public interface CountryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertCountry(Country country);
+
     @Query("SELECT * FROM country")
     List<Country> getAllCountries();
 
@@ -18,9 +22,6 @@ public interface CountryDao {
 
     @Query("SELECT EXISTS(SELECT * FROM country WHERE country = :country)")
     boolean isCountryExist(String country);
-
-    @Insert
-    void insertCountry(Country country);
 
     @Delete
     void deleteCountry(Country country);
